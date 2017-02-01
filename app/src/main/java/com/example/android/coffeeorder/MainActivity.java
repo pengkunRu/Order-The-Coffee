180818467877fcbbb06e771f7d48381559ec6673
rupengkun <522523828@qq.com>
2017-02-01 20:54:55 +0800

    "+"按钮对咖啡订单数量+1; "-"按钮对咖啡订单数量-1;

--- app/src/main/java/com/example/android/coffeeorder/MainActivity.java
+++ app/src/main/java/com/example/android/coffeeorder/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +19,23 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked
      */
     public void submitOrder(View view){
-        int quantity = 5;
-        display(quantity);
         displayPrice(quantity * 5);
+    }
+
+    /**
+     * This method is called when the increment button is clicked
+     */
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the decrement button is clicked
+     */
+    public void decrement(View view){
+        quantity = quantity - 1;
+        display(quantity);
     }
 
     /**
